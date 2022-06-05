bad7f98be93b9e1d916d3e907f05e51400121230
Brandon Shew <brandonshew98@gmail.com>
2022-06-05 12:07:51 -0400

    Autocomplete test app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import { Autocomplete } from '@react-google-maps/api';
+
 
 function App() {
+  const [autocomplete, setAutocomplete] = useState(null)
+  const onLoad = (autoC) => setAutocomplete(autoC)
+
+  const onPlaceChanged = () => {
+    if (autocomplete !== null) {
+      console.log(autocomplete)
+    } else {
+      console.log('Autocomplete is not loaded yet!')
+    }
+  }
   return (
     <div className="App">
+      <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+        <input
+              type="text"
+              placeholder="Customized your placeholder"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `240px`,
+                height: `32px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+                position: "absolute",
+                left: "50%",
+                marginLeft: "-120px"
+              }}
+            />
+            </Autocomplete>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
